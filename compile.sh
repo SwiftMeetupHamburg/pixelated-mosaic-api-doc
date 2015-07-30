f620bad04bdf69c9c3a69de1ee5a700f9bfcf0aa
Benedikt Terhechte <terhechte@gmail.com>
2015-07-30 10:18:53 +0200

    Changed a couple of model issues, improved the compile script.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,7 @@
-java -jar ./swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i ./pixelated-api.json -t ./swagger-codegen/modules/swagger-codegen/src/main/resources/swift -l swift -o $1
+if [ "$#" -ne 1 ]
+then
+    echo "Usage: compile.sh /folder/to/write/to"
+    exit 1
+fi
+rm -rf $1
+java -jar ./swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i ./editor.swagger.yaml -t ./swagger-codegen/modules/swagger-codegen/src/main/resources/swift -l swift -o $1
